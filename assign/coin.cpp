7a4788548cfc709a049974d83e1c1e8536fba319
0PP415 <majw0415@gmail.com>
2025-09-24 11:52:09 +0900

    Solved Coin!!!!

--- assign/coin.cpp
+++ assign/coin.cpp
@@ -2,40 +2,40 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <cmath>
 
 using namespace std;
 
 struct Data {
     int time;
-    float value;    
+    double value;    
 };
 
 struct minMAX {
-    float min;
-    float max;
+    double min;
+    double max;
 };
 
 vector<Data> datas;
 vector<minMAX> tree;
 
 minMAX operator + (const minMAX& mm1, const minMAX& mm2) {
-    float a = min(mm1.min, mm2.min);
-    float b = max(mm1.max, mm2.max);
+    double a = min(mm1.min, mm2.min);
+    double b = max(mm1.max, mm2.max);
     
     return {a, b};
 }
 
 minMAX tree_init(int start, int end, int index) {
     if (start == end) {
-        float tmp = datas[start].value;
-        return tree[index] = {tmp, tmp};
+        return tree[index] = {datas[start].value, datas[start].value};
     }
     int mid = (start + end) / 2;
     return tree[index] = tree_init(start, mid, index*2) + tree_init(mid+1, end, index*2 + 1); 
 }
 
 minMAX tree_query(int start, int end, int index, int q_begin, int q_end) {
-    if (q_begin > end || q_end < start) return {numeric_limits<float>::infinity(),-1.0f};
+    if (q_begin > end || q_end < start) return {numeric_limits<double>::infinity(),-1.0};
 
     if (q_begin <= start && end <= q_end) return tree[index];
 
@@ -44,15 +44,27 @@ minMAX tree_query(int start, int end, int index, int q_begin, int q_end) {
 
 }
 
+int get_index(int time) {
+    auto itr = upper_bound(datas.begin(), datas.end(), time, 
+        [](int time_val, const Data& data_element) {
+            return time_val < data_element.time;
+    });
+
+    if (itr == datas.begin()) return 0;
+    itr--;
+
+    return itr - datas.begin();
+}
+
 int main() {
     FILE* file = fopen("Pitcoin.txt", "r");
 
     int N;
     int time;
-    float value;
+    double value;
 
     fscanf(file, "%d", &N);
-    while (fscanf(file, "%d %f", &time, &value) == 2) {
+    while (fscanf(file, "%d %lf", &time, &value) == 2) {
         datas.push_back({time, value});
     }
     fclose(file);
@@ -68,8 +80,11 @@ int main() {
         int t_begin, t_end;
         scanf("%d %d", &t_begin, &t_end);
 
-        // query_result = tree_query(0, N-1, 1, begin, end);
-        printf("%.3f %.3f\n", query_result.min, query_result.max);
+        int i_begin = get_index(t_begin);
+        int i_end = get_index(t_end);
+
+        query_result = tree_query(0, N-1, 1, i_begin, i_end);
+        printf("%.3lf %.3lf\n", query_result.min, query_result.max);
     }
 
     return 0;
