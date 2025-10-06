e477002d04444c4af333f9a82bc4f2869830270b
0PP415 <majw0415@gmail.com>
2025-10-07 01:03:02 +0900

    Create callseq.cpp

---
+++ assign/callseq.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <map>
+
+using namespace std;
+
+int main(void) {
+    int N, k1, k2;
+    cin >> N >> k1 >> k2;
+
+    vector<string> seq;    
+    map<string, vector<string>> functions;
+
+    return EXIT_SUCCESS;
+}
