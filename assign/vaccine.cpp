9bb2ef3ee4a61aec003048060cc6cf7294731813
0PP415 <majw0415@gmail.com>
2025-10-08 23:14:22 +0900

    Create vaccine.cpp

---
+++ assign/vaccine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+
+    return EXIT_SUCCESS;
+}
